79eae0ecdcb798c8f2ad13587f571149f5d67427
Helloimalex1223 <plattaalex@gmail.com>
2024-04-27 12:07:58 -0900

    Add logic to only allow two pairs of numbers on the screen at once

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let resultNum = "";
 let operand = "";
 let operandClick = 0;
 
+
 function add(a, b)
 {
     return (a + b);
@@ -65,6 +66,7 @@ let num2Span = document.createElement("span");
 //Sets updates the calculator's DOM with the relevant button id when a button is clicked
 document.addEventListener("click", function(e)
 {
+    console.log(`FIRST VALUE IS ${num1}, SECOND VALUE IS ${num2}, RESULT IS ${resultNum}`);
     if(e.target.tagName === "BUTTON")
     {
         //clear values if the user clicks clear
@@ -75,6 +77,15 @@ document.addEventListener("click", function(e)
             resultNum = "";
             operand = "";
             display.textContent = "";
+        }
+        
+        //requires both values to be entered before allowing the user to calculate the answer
+        if(e.target.id === "=")
+        {
+            if(num1 !== "" && num2 !== "")
+            {
+                equals();
+            }
         }
 
         //skip this section if an operand button is pressed
@@ -108,69 +119,56 @@ document.addEventListener("click", function(e)
         //if the buttons are operators, set the operand value to their id
         else
         {
-        operandClick++;
-        evaluateTwoNumbers(operandClick);
-        
+
+        if(operandClick > 0)
+        {
+            operandClick = 0;
+            equals();
+            result = num1;
+        }
+
+        operandClick++;    
         if(e.target.id === "multiply")
         {
             operand = "*"
-            operandSpan.textContent = "*";
+
         }
         else if(e.target.id === "divide")
         {
             operand = "/"
-            operandSpan.textContent = "/";
         }
         else if(e.target.id === "subtract")
         {
             operand = "-"
-            operandSpan.textContent = "-";
         }
         else if(e.target.id === "add")
         {
             operand = "+"
-            operandSpan.textContent = "+";
         }
-        else if(e.target.id === "=")
-        {
-            equals();
+
         }
         //if the buttons are numbers, set the relevant number value to the button's ID. Display it on the screen
         display.textContent = `${num1} ${operand} ${num2}`;
-
-        }
     }
 })
-//******************************************** */
-//TODO: Fix logic around evaluating two numbers
-//******************************************** */
-function evaluateTwoNumbers(numClick)
-{
-    if(numClick > 1)
-    {
-        equals();
-        console.log(operand);
-        numClick = 0;
-    }
-}
+
 
 //separate equals function. Needed this to call if an operand is already defined and the user clicks an operand button again. Calculator can only display two pairs of numbers at once
 function equals()
 {
+    
     let result = operate(operand, num1, num2);
     resultNum = result;
-    //converts result to number with only two decimal places
-    let resConvert = parseFloat(resultNum).toFixed(2);
+    // //converts result to number with only two decimal places
+    let resConvert = +resultNum.toFixed(2);
     Number(resConvert);
-    
 
-    //Sets the num1 variable to the result. Num2 and operand variables are reset after converting them back to a string
-    num2.toString();
+    // //Sets the num1 variable to the result. Num2 and operand variables are reset after converting them back to a string
     num1 = resConvert;
     num2 = "";
     operand = "";
-    operandSpan.textContent = "";  
-    console.log(`Number 1 - ${num1} result -  ${resultNum}`); 
+    operandSpan.textContent = "";
+    operandClick = 0;
 }
 
 
