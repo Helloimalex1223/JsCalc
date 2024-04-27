31125e44afda856bc5ce8d5bc19b0e66a83dc4ad
Helloimalex1223 <plattaalex@gmail.com>
2024-04-27 08:04:28 -0900

    Add basic logic to evaluate two pairs of numbers

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let num2 = "";
 let displayNum = "";
 let resultNum = "";
 let operand = "";
-
+let operandClick = 0;
 
 function add(a, b)
 {
@@ -61,17 +61,24 @@ let num1Span = document.createElement("span");
 let operandSpan = document.createElement("span");
 let num2Span = document.createElement("span");
 
+
 //Sets updates the calculator's DOM with the relevant button id when a button is clicked
 document.addEventListener("click", function(e)
 {
     if(e.target.tagName === "BUTTON")
-    
     {
-        console.log(`First variable is ${num1}`);
-        console.log(`Second variable is ${num2}`);
-        console.log(`Result variable is ${resultNum}`);
+        //clear values if the user clicks clear
+        if(e.target.id === "clear")
+        {
+            num1 = "";
+            num2 = "";
+            resultNum = "";
+            operand = "";
+            display.textContent = "";
+        }
+
         //skip this section if an operand button is pressed
-        if(e.target.className !== "operandButtons")
+        else if(e.target.className !== "operandButtons")
         {
         //if the result value is not empty (meaning the user did one calculation), the num1 defaults to the result value. The user can then do another operation on the result number
         //if the operand is defined, update the second number
@@ -96,15 +103,14 @@ document.addEventListener("click", function(e)
                 num1Span.textContent = num1;
                 display.appendChild(num1Span);
             }
-
-            console.log(`*****************************`);
-            console.log(`The equation is ${num1} ${operand} ${num2}, with a result of ${resultNum}`)
         }
 
         //if the buttons are operators, set the operand value to their id
-        
         else
         {
+        operandClick++;
+        evaluateTwoNumbers(operandClick);
+        
         if(e.target.id === "multiply")
         {
             operand = "*"
@@ -124,21 +130,10 @@ document.addEventListener("click", function(e)
         {
             operand = "+"
             operandSpan.textContent = "+";
-            console.log(`The equation is ${num1} ${operand} ${num2}, with a result of ${resultNum}`);
         }
         else if(e.target.id === "=")
         {
-            let result = operate(operand, num1, num2);
-            resultNum = result;
-            Number(resultNum);
-            display.textContent = result;
-
-            //Sets the num1 variable to the result. Num2 and operand variables are reset after converting them back to a string
-            num2.toString();
-            num1 = resultNum;
-            num2 = "";
-            operand = "";
-            operandSpan.textContent = "";
+            equals();
         }
         //if the buttons are numbers, set the relevant number value to the button's ID. Display it on the screen
         display.textContent = `${num1} ${operand} ${num2}`;
@@ -146,5 +141,36 @@ document.addEventListener("click", function(e)
         }
     }
 })
+//******************************************** */
+//TODO: Fix logic around evaluating two numbers
+//******************************************** */
+function evaluateTwoNumbers(numClick)
+{
+    if(numClick > 1)
+    {
+        equals();
+        console.log(operand);
+        numClick = 0;
+    }
+}
+
+//separate equals function. Needed this to call if an operand is already defined and the user clicks an operand button again. Calculator can only display two pairs of numbers at once
+function equals()
+{
+    let result = operate(operand, num1, num2);
+    resultNum = result;
+    //converts result to number with only two decimal places
+    let resConvert = parseFloat(resultNum).toFixed(2);
+    Number(resConvert);
+    
+
+    //Sets the num1 variable to the result. Num2 and operand variables are reset after converting them back to a string
+    num2.toString();
+    num1 = resConvert;
+    num2 = "";
+    operand = "";
+    operandSpan.textContent = "";  
+    console.log(`Number 1 - ${num1} result -  ${resultNum}`); 
+}
 
 
